601dea9bb1356061568ce82c6d6ac6799afc1104
Bablu001 <bablushaikh703854@gmail.com>
2025-06-23 18:23:25 +0530

    updated ec2.tf

--- Terraform/ec2.tf
+++ Terraform/ec2.tf
@@ -1,9 +1,14 @@
 resource "aws_instance" "wanderlust" {
-  ami                         = "ami-007020fd9c84e18c7"  # Ubuntu 22.04 LTS in ap-south-1
+  ami                         = "ami-0f918f7e67a3323f0"  # Ubuntu 22.04 LTS in ap-south-1
   instance_type               = var.instance_type
   subnet_id                   = aws_subnet.public[0].id
   vpc_security_group_ids      = [aws_security_group.ec2_sg.id]
   associate_public_ip_address = true
+
+  root_block_device {
+    volume_size = 29
+    volume_type = "gp3"
+  }
 
   user_data = <<-EOF
               #!/bin/bash
@@ -11,8 +16,6 @@ resource "aws_instance" "wanderlust" {
               apt install -y software-properties-common
               add-apt-repository --yes --update ppa:ansible/ansible
               apt install -y ansible
-              systemctl enable --now ssh
-              ansible --version > /tmp/ansible_version.txt
               EOF
 
   tags = {
